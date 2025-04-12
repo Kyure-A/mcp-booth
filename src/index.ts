26599b8a319eea9a142d5c8db35054ab9b8c1070
Kyure_A <49436968+Kyure-A@users.noreply.github.com>
2025-04-13 03:21:11 +0900

    chore: add main

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { createSearchTools } from "./tools/search";
 
-// サーバーインスタンスの作成
 export const server = new McpServer({
     name: "Booth.pm",
     version: "0.1.0",
+});
+
+createSearchTools(server);
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    
+    console.error("MCP Server running on stdio");
+}
+
+main().catch((error) => {
+    console.error("Fatal error in main():", error);
+    process.exit(1);
 });
